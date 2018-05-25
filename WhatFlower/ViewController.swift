3b4e7ac3b8b7b2117ec863e1de7e9ad94a8aa304
Jerry Ding <jerryding@jerrys-mbp.sgs.local>
2018-05-25 14:04:55 -0700

    Initial Commit

---
+++ WhatFlower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Jerry Ding on 2018-05-25.
+//  Copyright © 2018 Jerry Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
